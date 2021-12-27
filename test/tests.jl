a7d192588f0bd619a752c7e5b662002776b812c1
Philip Stuckey <pstuc002@gmail.com>
2021-12-27 09:56:02 -0500

    added some placeholders for tests

--- test/tests.jl
+++ test/tests.jl
@@ -1,4 +1,14 @@
 
 @testset "makes sure some easy examples make sense" begin
-	@test find_route() == [0, 1]
+	@testset "if the origin and destonation are within range, there should be no stops" begin
+		@test find_route() == [0, 1]
+	end
+
+	@testset "if the origin and destonation are out of range, but there is a stop in between, then a path with one stop should exist" begin
+		@test false
+	end
+
+	@testset "if there are multiple 1-stop routes, then the cheepest one should be selected" begin
+		@test false
+	end
 end
